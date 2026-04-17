da424f2d472c14688dd3c4b8f7cd9cac188e0204
Aayushdubey05 <dubeyaayush333@gmail.com>
2026-04-17 11:26:21 +0530

    Pushing the code whole code

--- College-ManageMent-System/src/app/view/view-routing.module.ts
+++ College-ManageMent-System/src/app/view/view-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DepartmentComponent } from './role/admin/department/department.component';
+import { ClassroomComponent } from './role/admin/classroom/classroom.component';
+import { InfrastructureComponent } from './role/admin/infrastructure/infrastructure.component';
+import { ExamComponent } from './role/admin/exam/exam.component';
 
 const routes: Routes = [
   { path: 'department', component: DepartmentComponent },
+  { path: 'classroom', component: ClassroomComponent },
+  { path: 'infrastructure', component: InfrastructureComponent },
+  { path: 'exam', component: ExamComponent },
 ];
 
 @NgModule({
